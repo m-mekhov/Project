a0cb0057c23cd461ff535f4fbc2568d49681f7f6
Mekhov Maksim <mekhov.m@bk.ru>
2022-11-29 16:07:20 +0300

    Сделан интерфейс для теста

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.BackColor = System.Drawing.Color.White;
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.BackColor = System.Drawing.Color.White;
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.BackColor = System.Drawing.Color.White;
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            resources.ApplyResources(this.radioButton4, "radioButton4");
+            this.radioButton4.BackColor = System.Drawing.Color.White;
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = false;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        int question_count; // Кол-во вопросов
+        int balls;
+        string result;
+
+        StreamReader Read;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Завершить")
+            {
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                button2.Visible = false;
+                label2.Visible = false;
+
+                if(balls >= 0 && balls <=9)
+                {
+                    result = "У вас отсутсвие депрессивных симптомов";
+                }
+
+                label1.Text= String.Format("Тестирование завершено.\n" + "Количество набранных баллов: {0}.\n" + "Результат: {1}", balls, result);
+
+            }
+            if (button2.Text == "Следующий вопрос")
+            {
+                count();
+                question();
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Выйти";
+            button2.Text = "Следующий вопрос";
+            radioButton1.CheckedChanged += new EventHandler(switching_status);
+            radioButton2.CheckedChanged += new EventHandler(switching_status);
+            radioButton3.CheckedChanged += new EventHandler(switching_status);
+            radioButton4.CheckedChanged += new EventHandler(switching_status);
+
+
+            start();
+        }
+        void start()
+        {
+            var encoding = System.Text.Encoding.GetEncoding(65001);
+
+            try
+            {
+                Read = new StreamReader(Directory.GetCurrentDirectory() + @"\Question.txt", encoding);
+
+                this.Text = "Психологический тест";
+
+                question_count = 1;
+                balls = 0;
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error 1");
+            }
+            question();
+        }
+        public void question()
+        {
+            label1.Text = "Как вы чувствовали себя на этой неделе?";
+            label2.Text = String.Format("Номер вопроса: {0}/21", question_count);
+            radioButton1.Text = Read.ReadLine();
+            radioButton2.Text = Read.ReadLine();
+            radioButton3.Text = Read.ReadLine();
+            radioButton4.Text = Read.ReadLine();
+
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+
+            question_count = question_count + 1;
+
+
+            if (Read.EndOfStream == true) button2.Text = "Завершить";
+        }
+        public void switching_status(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Focus();
+            RadioButton p = (RadioButton)sender;
+            var t = p.Name;
+        }
+        public void count()
+        {
+            if (radioButton1.Checked == true)
+            {
+                balls = balls + 0;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                balls = balls + 1;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                balls = balls + 2;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                balls = balls + 3;
+            }
+        }
+    }
+}
